25abe200d9a8559985b29b2f8f7bc5216838c0ab
toriwat328 <toriwat328@gmail.com>
2020-01-13 10:41:37 -0500

    3rd try to tackle repeated string problem

--- app.js
+++ app.js
@@ -298,27 +298,51 @@
 // let s = 'abcac';
 // let n = 10;
 
-function repeatedString2(s, n){
-    let count = 0;
+// function repeatedString2(s, n){
+//     //defining the variable count and setting it equal to 0
+//     //this is increase everytime we see an 'a' in the substring
+//     let count = 0;
+//
+//     //looping thru the string to see if there is an a. If so we increase the count.
+//     for(let i = 0; i < s.length; i++){
+//         if(s.charAt(i) == 'a'){
+//             count++;
+//         }
+//     }
+//
+//     //dividing the wanted length of the substring 'n' by the length of the actual string 's' and saving to variable
+//
+//     //indicating how many times we need to repeat s?
+//     let div = n / s.length; // 2
+//
+//     //taking the modulus of substring length with string length and setting the remainder to a variable
+//     let reminder = n % s.length; // 0
+//
+//     //since div represents how many time we need to repeat the string, we redefine count to make it equal to div multiplied by the count of 'a's in s.
+//     count = div * count;
+//
+//
+//     //this final for loop takes care of issue of having n not be divisible by s.length. we are looping through the reminder to see if there is 'a', if so we add this to the overall count.
+//     for(let i = 0; i < reminder; i++){
+//         if(s.charAt(i) == 'a'){
+//             count++;
+//         }
+//     }
+//     let finalCount = Math.floor(count)
+//     return finalCount;
+// }
+//
+// console.log(repeatedString2('aba', 10));
 
-    for(let i = 0; i < s.length; i++){
-        if(s.charAt(i) == 'a'){
-            count++;
-        }
-    }
+function repeatedString3(s, n){
 
-    let div = n / s.length; // 2
-    let reminder = n % s.length; // 0
+    const counta = (str) => str.split('')
+    .filter(c => c == 'a')
+    .length
 
-    count = div * count;
-
-    for(let i = 0; i < reminder; i++){
-        if(s.charAt(i) == 'a'){
-            count++;
-        }
-    }
-
-    return count;
+  const d = Math.floor(n / s.length)
+  const r = n - (s.length * d)
+  return d * counta(s) + counta(s.substr(0, r))
 }
 
-console.log(repeatedString2('abcac', 10));
+console.log(repeatedString3('aba', 10));
