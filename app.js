10458646069c5a64961e001c41ce9ef5080077a8
toriwat328 <toriwat328@gmail.com>
2020-01-16 11:27:37 -0500

    finished ransom note

--- app.js
+++ app.js
@@ -449,31 +449,42 @@
 
 const checkMagazines = (magazine, note) => {
     //magazine is a hashtable
+    if(magazine.length < note.length){
+        return 'No';
+    }
+
     let wordcount = 0;
+
+    let magazine1 = magazine.sort()
+    let note1 = note.sort()
 
     const mag = new Map();
 
-    for(let i = 0; i < magazine.length; i++){
-        mag.set(magazine[i], i);
+    for(let i = 0; i < magazine1.length; i++){
+        mag.set(magazine1[i], i);
     }
 
     console.log(mag);
 
-    for(let i = 0; i < note.length; i++){
-        if(mag.has(note[i])){
-            wordcount++;
-            mag.delete(note[i])
+    for(let i = 0; i < note1.length; i++){
+        if(wordcount === note1.length){
+            break;
         }
+        if(mag.has(note1[i])){
+            wordcount++;
+            mag.delete(note1[i]);
+        }
+
     }
 
-    if(wordcount !== note.length){
+    if(wordcount !== note1.length){
         return 'No';
     } else {
         return 'Yes';
     }
 }
 
-console.log(checkMagazines(['give', 'me', 'one', 'grand', 'today', 'night'], ['give', 'one', 'grand', 'today']));
+// console.log(checkMagazines(['give', 'me', 'one', 'grand', 'today', 'night'], ['give', 'one', 'grand', 'today']));
 
 // console.log(checkMagazines(['two', 'times', 'three', 'is', 'not', 'four'], ['two', 'times', 'two', 'is', 'four']));
 
