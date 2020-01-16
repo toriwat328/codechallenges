9a7dacff41758f04936b47dbd9508709c2e0e2a0
toriwat328 <toriwat328@gmail.com>
2020-01-16 07:27:01 -0500

    optimized code for time complexity

--- app.js
+++ app.js
@@ -396,16 +396,20 @@ const twoString = (s1, s2) => {
     const string1 = s1.split('')
     const string2 = s2.split('')
 
+    const mySet = new Set()
+
+    for(let i = 0; i < string1.length; i++){
+        if(!mySet.has(string1[i])){
+            mySet.add(string1[i]);
+        }
+    }
 
     let count = 0;
 
-    for(let i = 0; i < string1.length; i++){
-        for(let j = 0; j < string2.length; j++ ){
-            if(string1[i] === string2[j]){
-                count++;
-                return 'YES'
-
-            }
+    for(let i = 0; i < string2.length; i++){
+        if(mySet.has(string2[i])){
+            count++;
+            return 'YES'
         }
     }
 
@@ -414,4 +418,4 @@ const twoString = (s1, s2) => {
     }
 }
 
-console.log(twoString('hi', 'world'));
+console.log(twoString('hello', 'world'));
