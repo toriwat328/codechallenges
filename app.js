0ab73275eb161280d88d078414fa53b27df12289
toriwat328 <toriwat328@gmail.com>
2020-01-17 22:31:25 -0500

    started bubblesort coding challenge

--- app.js
+++ app.js
@@ -447,45 +447,59 @@
 //     }
 // }
 
-const checkMagazines = (magazine, note) => {
-    //magazine is a hashtable
-    if(magazine.length < note.length){
-        return 'No';
-    }
-
-    let wordcount = 0;
-
-    let magazine1 = magazine.sort()
-    let note1 = note.sort()
-
-    const mag = new Map();
-
-    for(let i = 0; i < magazine1.length; i++){
-        mag.set(magazine1[i], i);
-    }
-
-    console.log(mag);
-
-    for(let i = 0; i < note1.length; i++){
-        if(wordcount === note1.length){
-            break;
-        }
-        if(mag.has(note1[i])){
-            wordcount++;
-            mag.delete(note1[i]);
-        }
-
-    }
-
-    if(wordcount !== note1.length){
-        return 'No';
-    } else {
-        return 'Yes';
-    }
-}
+// const checkMagazines = (magazine, note) => {
+//     //magazine is a hashtable
+//     if(magazine.length < note.length){
+//         return 'No';
+//     }
+//
+//     let wordcount = 0;
+//
+//     let magazine1 = magazine.sort()
+//     let note1 = note.sort()
+//
+//     const mag = new Map();
+//
+//     for(let i = 0; i < magazine1.length; i++){
+//         mag.set(magazine1[i], i);
+//     }
+//
+//     console.log(mag);
+//
+//     for(let i = 0; i < note1.length; i++){
+//         if(wordcount === note1.length){
+//             break;
+//         }
+//         if(mag.has(note1[i])){
+//             wordcount++;
+//             mag.delete(note1[i]);
+//         }
+//
+//     }
+//
+//     if(wordcount !== note1.length){
+//         return 'No';
+//     } else {
+//         return 'Yes';
+//     }
+// }
 
 // console.log(checkMagazines(['give', 'me', 'one', 'grand', 'today', 'night'], ['give', 'one', 'grand', 'today']));
 
 // console.log(checkMagazines(['two', 'times', 'three', 'is', 'not', 'four'], ['two', 'times', 'two', 'is', 'four']));
 
 // console.log(checkMagazines(['ive', 'got', 'a', 'lovely', 'bunch', 'of', 'coconuts'], ['ive', 'got', 'some', 'coconuts']));
+
+const bubblesort = () => {
+    let numOfSwaps = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                
+            }
+        }
+    }
+}
