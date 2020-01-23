08b7f8cd0abaf5a9ffdc012b045e7e2a4e4b5065
toriwat328 <toriwat328@gmail.com>
2020-01-23 15:37:17 -0500

    commented out previous problem

--- app.js
+++ app.js
@@ -712,31 +712,38 @@
 //
 // console.log(alternatingCharacters('ABABABAB'));
 
-const alternatingCharacters = (s) => {
-    let deletions = 0;
-    let string = s.split('');
-    console.log(string);
-
-
-
-        let i = 0;
-
-        const test = () => {
-            if(string[i] === string[i + 1]){
-                console.log(`to be removed: ${string[i]}, ${i}`);
-                string.splice(i, 1);
-                deletions++;
-                i = i - 1
-            }
-            i++
-        }
-
-
-
-
-
-
-    return deletions;
-}
-
-console.log(alternatingCharacters('ABABABAB'));
+// const alternatingCharacters = (s) => {
+//     let deletions = 0;
+//     let string = s.split('');
+//     console.log(string);
+//
+//     if(i === string.length-1){
+//         return deletions;
+//     }
+//
+//
+//     if(i < string.length-1){
+//
+//     }
+//
+//         // let i = 0;
+//         //
+//         // const test = () => {
+//         //     if(string[i] === string[i + 1]){
+//         //         console.log(`to be removed: ${string[i]}, ${i}`);
+//         //         string.splice(i, 1);
+//         //         deletions++;
+//         //         i = i - 1
+//         //     }
+//         //     i++
+//         // }
+//
+//
+//
+//
+//
+//
+//     return deletions;
+// }
+//
+// console.log(alternatingCharacters('ABABABAB'));
