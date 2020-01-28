91f46731dd82b590560192bc6d28fff376008243
toriwat328 <toriwat328@gmail.com>
2020-01-28 18:46:32 -0500

    fibonacci sequence problem

--- app.js
+++ app.js
@@ -747,3 +747,11 @@
 // }
 //
 // console.log(alternatingCharacters('ABABABAB'));
+
+
+const fibonacci = (n) => {
+    if(n == 0 || n == 1){
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
