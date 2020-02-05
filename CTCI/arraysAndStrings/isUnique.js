a8f9d77b3de1a404bdc1e0b1cec3b2b1f6d5c086
toriwat328 <toriwat328@gmail.com>
2020-02-05 08:56:36 -0500

    started isUnique problem

---
+++ CTCI/arraysAndStrings/isUnique.js
@@ -0,0 +1,7 @@
+//Arrays and Strings
+
+//is Unique: Implement an algorithm to determine if a string has all unique characters. What if you can not use an additional data structure.
+
+const isUnique = () => {
+    
+}
