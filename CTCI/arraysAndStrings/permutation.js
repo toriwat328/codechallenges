90e0af80d637151d036a6143506fdbbc04d5cb74
toriwat328 <toriwat328@gmail.com>
2020-02-06 08:15:43 -0500

    reorganizing file names

---
+++ CTCI/arraysAndStrings/permutation.js
@@ -0,0 +1,11 @@
+//Check Permutation: Given two strings, write a method to decide if one is a permutation of the other
+
+//Is the permutation comparison is case sensitive or is whitespace significant
+
+//strings of different lengths can not be permutations of each other
+
+
+//Sort the strings -> permutations have the same characters in a different order so if you sort them, and they are the same then they are permutations
+
+
+//Check if the two strings have identical character counts -> Can iterate through the string counting how many times each characters appears. Then afterwards compare the new string. 
