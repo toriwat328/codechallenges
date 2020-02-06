d81d30f652b8ecb3775a50d91c0a8afd3e91a0b3
toriwat328 <toriwat328@gmail.com>
2020-02-06 15:54:51 -0500

    finished permutation problem by sort and compare

--- CTCI/arraysAndStrings/permutation.js
+++ CTCI/arraysAndStrings/permutation.js
@@ -7,5 +7,20 @@
 
 //Sort the strings -> permutations have the same characters in a different order so if you sort them, and they are the same then they are permutations
 
+const permutationSort = (str1, str2) => {
+    let strsort1 = str1.split('').sort().join('');
+    let strsort2 = str2.split('').sort().join('');
 
-//Check if the two strings have identical character counts -> Can iterate through the string counting how many times each characters appears. Then afterwards compare the new string. 
+    console.log(strsort1);
+    console.log(strsort2);
+
+    if(strsort1 !== strsort2){
+        return false;
+    }else {
+        return true;
+    }
+}
+
+console.log(permutationSort('racecar', 'carrac'));
+
+//Check if the two strings have identical character counts -> Can iterate through the string counting how many times each characters appears. Then afterwards compare the new string.
