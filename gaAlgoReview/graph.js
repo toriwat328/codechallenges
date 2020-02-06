dab3b214b3fce4e63b7fed3be435fcadb87e76e5
toriwat328 <toriwat328@gmail.com>
2020-02-06 11:44:00 -0500

    finished figuring out find connection function for the graph algo

--- gaAlgoReview/graph.js
+++ gaAlgoReview/graph.js
@@ -37,37 +37,37 @@ node5.friends.push(node1);
 // console.log(util.inspect(node1, {showHidden: false, depth: null, colors:true}));
 
 
-//find node with a name with node and node and name as parameters
+//find if node is connected to name?
 const find = (node, name) => {
-    //iterate through names who have visited 
-    for(let name of visited) {
-        //if the name arg is the same as the node's name property, return false
-        if(name === node.name){
-            console.log('1');
+    //establishing a check to understand what nodes we have visited to determine if there is a connection - hence visited array
+    for(let fname of visited) {
+        //if the node name is in the visited array that means that it was visited so we do not need to visit it again, thus returning false
+        if(fname === node.name){
+            console.log(fname);
             return false;
         }
     }
-    //push node.name to visited array
+    //for node names that have not been visited, we push into the visited array
     visited.push(node.name);
 
-    //if node name arg is equal to name, return true
+    //if the node's name equals the name that we are trying to find then return true
     if(node.name === name){
-        console.log('2');
+        console.log(node.name);
         return true;
     } else {
-        //otherwise iterate through node's friends. if find friend return true
+        //if the node's name does not equal the name that we are trying to find, go through the node's friends through the find function until the node's name equal the name that we are searching for. If that is ultimately true making the if statement true, it the function will return true
         for (let friend of node.friends) {
             if(find(friend, name)){
                 console.log('3');
                 return true;
             }
         }
-        //otherwise return false
+        //otherwise if we can not find a connection between the node and the name we are searching for, return false
         console.log('4');
         return false;
     }
 }
 
-console.log(node1);
-console.log(node2);
-console.log(find(node1, 'Zagthorp'));
+console.log(node3);
+console.log(find(node1, 'Ellen'));
+console.log(visited);
