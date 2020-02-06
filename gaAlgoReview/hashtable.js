a15d8fe47641b29321961dce37645991e14a0f1a
toriwat328 <toriwat328@gmail.com>
2020-02-06 10:16:31 -0500

    added hashtable find function to the hashtable implementations

--- gaAlgoReview/hashtable.js
+++ gaAlgoReview/hashtable.js
@@ -39,10 +39,44 @@ const insert = (newPerson) => {
   if(contents[index] === undefined){
       contents[index] = newPerson;
   } else {
+      //if there is something defined at that index, set a variable currentPersonInTable equal to that index value.
       let currentPersonInTable = contents[index];
+      //while currentPersonInTable next property is defined, set currentPersonInTable to currentPersonInTable.next -> traversing the linked list until they find the tail of the linked list
       while(currentPersonInTable.next !== undefined){
           currentPersonInTable = currentPersonInTable.next;
       }
+      //set currentPersonInTable.next that is undefined equal to the newPerson object
       currentPersonInTable.next = newPerson;
   }
 }
+
+insert(person1);
+insert(person2);
+insert(person3);
+insert(person4);
+
+console.log(contents[406]);
+//in terminal the hash code looks like this
+//[
+//<275 empty items>
+//{name: 'Bob', age: 12},
+//<130 empty items>
+//{ name: 'Matt', age: 39, next: {name: 'attM', age: 4000, next: {name: 'atMt', age: 100}}}
+//]
+
+//function to find a name within contents, Add name string as a parameter
+const find = (name) => {
+    //put name into the hash function to find the index
+  const index = hash(name);
+  //set variable foundperson = to context at the found index
+  let foundPerson = contents[index];
+  //while name is not equal to foundPerson.name. set found person = to foundperson.next to traverse through the linked list of people with the same hashcode and index
+  while(name !== foundPerson.name){
+      foundPerson = foundPerson.next;
+  }
+  //return the found person that is equal name
+  return foundPerson;
+}
+
+console.log(find('atMt'));
+//{name: 'atMt', age: 100}
